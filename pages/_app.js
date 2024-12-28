38f29b2519d1aff704f9c71e902ea1bd4d5035f7
emha arch <mhmmdmhthr@gmail.com>
2024-12-29 02:59:30 +0700

    update meta project

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,13 @@ import Head from "next/head";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CircularProgress from "@mui/material/CircularProgress";
+
 import { wrapper } from "redux/store";
 import { verifyToken } from "redux/slice/auth";
+
 import { isBrowser } from "src/utils";
 
 import "styles/globals.css";
@@ -14,28 +17,18 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-// Updated theme to match your custom colors
 const theme = createTheme({
   palette: {
     type: "dark",
     primary: {
       main: "#6C63FF",
-      light: "#8B85FF",
     },
     secondary: {
       main: "#845EC2",
     },
-    accent: {
-      main: "#00C9A7",
-    },
-    background: {
-      default: "#0F1624",
-      paper: "#171E31",
-    },
-    text: {
-      primary: "#FFFFFF",
-      secondary: "#B8C1EC",
-    },
+    body: "#0F1624",
+    text: { main: "#FFFFFF" },
+    bodyBg: { main: "#0F1624" },
   },
 });
 
@@ -60,8 +53,8 @@ function App({ Component, pageProps }) {
 
   if (loading) {
     return (
-      <div className="grid place-items-center h-screen bg-bodyBg">
-        <CircularProgress className="text-primary" />
+      <div className="grid place-items-center h-screen">
+        <CircularProgress />
       </div>
     );
   }
@@ -69,47 +62,39 @@ function App({ Component, pageProps }) {
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
       <ThemeProvider theme={theme}>
-        <div className="min-h-screen flex flex-col bg-bodyBg text-mainText">
-          <Head>
-            <title>Best online courses - Learn to succeed | learnlit</title>
-            <meta
-              name="description"
-              content="learnlit is an online learning and teaching marketplace with varied courses. Learn programming, software development, marketing, data science and more."
-            />
-            <meta
-              name="title"
-              content="Best online courses - Learn to succeed | learnlit"
-            />
-            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="/favicon-32x32.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="/favicon-16x16.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/apple-touch-icon.png"
-            />
-            <link rel="manifest" href="/site.webmanifest" />
-            <link
-              rel="mask-icon"
-              href="/safari-pinned-tab.svg"
-              color="#5bbad5"
-            />
-            <meta name="theme-color" content="#0F1624" />
-          </Head>
-          <main className="flex-1">
-            <Component {...pageProps} />
-          </main>
-        </div>
+        <Head>
+          <title>Best online courses - Learn to succeed | learnlit</title>
+          <meta
+            name="description"
+            content="learnlit is an online learning and teaching marketplace with varied courses. Learn programming, software development, marketing, data science and more."
+          />
+          <meta
+            name="title"
+            content="Best online courses - Learn to succeed | learnlit"
+          />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6C63FF" />
+          <meta name="theme-color" content="#0F1624" />
+        </Head>
+        <Component {...pageProps} />
       </ThemeProvider>
     </GoogleOAuthProvider>
   );
